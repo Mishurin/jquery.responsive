bda43a87f6a896129e8d9fc2f128685c75b806f5
Igor Mishurin <borlandpascal@mail.ru>
2015-12-13 21:59:13 +0300

    Dynamic prototype generation.

--- jquery.responsive.js
+++ jquery.responsive.js
@@ -1,12 +1,50 @@
 (function($) {
-    var STANDARD_MEDIA = [768, 992, 1200];
-    var Responsive = function (settings) {
-        var settings = $.extend({}, settings);
-        settings.media = settings.media? settings.media : STANDARD_MEDIA;
-        settings.resize = typeof settings.resize === 'boolean'? settings.resize : true;
-        return settings;
+    var DEFAULT_SETTINGS = {
+        breakpoints: [
+            {'mobile': 768},
+            {'tablet': 992},
+            {'desktop': 1200}
+        ],
+        resize: true
     };
+
+    function _isObject(val) {
+        if (val === null) { return false;}
+        return ( (typeof val === 'function') || (typeof val === 'object') );
+    }
+
+    function _capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    function _preparePrototypeFromArray(proto, breakpoints) {
+        var i = breakpoints.length;
+        while(i--) {
+            proto['is' + _capitalizeFirstLetter(Object.keys(breakpoints[i])[0])] = function() {};
+        }
+    }
+
+    function _preparePrototypeFromObject(proto, breakpoints) {
+        for(var breakpoint in breakpoints) {
+            proto['is' + _capitalizeFirstLetter(breakpoint)] = function() {};
+        }
+    }
+
+    var Responsive = function () {};
+
 	$.responsive = function(settings) {
-        if(settings) return new Responsive(settings);
+        var proto = Responsive.prototype = {};
+        var settings = $.extend({}, DEFAULT_SETTINGS, settings);
+        var breakpoints = settings.breakpoints;
+        if($.isArray(breakpoints)) {
+            _preparePrototypeFromArray(proto, breakpoints);
+        } else if(_isObject(breakpoints)) {
+            _preparePrototypeFromObject(proto, breakpoints);
+        } else {
+            breakpoints = DEFAULT_SETTINGS.breakpoints;
+            _preparePrototypeFromArray(proto, breakpoints);
+        }
+
+        return new Responsive(settings);
 	};
 }(jQuery));
--- tests/main.js
+++ tests/main.js
@@ -16,25 +16,27 @@ describe("JQuery Responsive Helpers Test Suit", function() {
         expect($.responsive({})).toBeObject();
     });
 
-    it("Settings object is required", function() {
-        expect($.responsive()).toBeUndefined();
+    it("Should return default isMobile method", function() {
+        expect($.responsive({}).isMobile).toBeFunction();
     });
 
-    it("Should return standard set when no media specified in object", function() {
-        expect($.responsive({}).media).toEqual([768, 992, 1200]);
+    it("Should return default isTablet method", function() {
+        expect($.responsive({}).isTablet).toBeFunction();
     });
 
-    it("Should return standard set when no media specified in object", function() {
-        var settings = {
-            media: [600, 700, 800]
-        };
-        expect($.responsive(settings).media).toEqual([600, 700, 800]);
-    });
-    it("Should return true resize option if it was not specified in settings", function() {
-        expect($.responsive({}).resize).toBeTruthy();
+    it("Should return default isTablet method", function() {
+        expect($.responsive({}).isDesktop).toBeFunction();
     });
 
-    it("Should return specified resize option", function() {
-        expect($.responsive({resize: false}).resize).toBeFalsy();
+    it("Should return default isMobile method on wrong data type", function() {
+        expect($.responsive({breakpoints: null}).isMobile).toBeFunction();
+    });
+
+    it("Should return custom method isA when breakpoints are set", function() {
+        expect($.responsive({breakpoints: [{'a': 100}]}).isA).toBeFunction();
+    });
+
+    it("Should return custom method isA when breakpoints set as a dictionary", function() {
+        expect($.responsive({breakpoints: {'a': 'a'}}).isA).toBeFunction();
     });
 });
